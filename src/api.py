e2d96e1bdefb60bdcab3dd7c0be753cfa7a31ced
vtoshio <vtoshio2004@gmail.com>
2025-03-01 22:28:56 -0300

    feat: adicionado um novo endpoint e ferramentas para customização de entrega de dados de usuários.

--- src/api.py
+++ src/api.py
@@ -1,48 +1,34 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 from fastapi.exceptions import HTTPException
-from .helpers import generate_user
+from .helpers import Generator
 import asyncio
 
 router = APIRouter()
 
+@router.get('/user')
+async def user(gender:str='any', safe:bool=True, location:str='pt-BR'):
+    '''
+    Endpoint da API que retorna os dados de um único usuário de acordo seu sexo.
+
+    Args:
+    - gender(str): define o sexo do usuário criado, sendo 'f', 'm', ou 'any'.
+    - safe(bool): se o e-mail gerado é um e-mail não oficial ou não.
+    - locale(str): define a localização de origem dos dados ('pt-BR', 'en-US', etc...)
+    '''
+    response = Generator.generate_user(gender, safe, location)    
+    return JSONResponse(response, status_code=200)
+
 @router.get('/users/')
-async def users(rows:int, location:str='pt-BR'):
+async def users(rows:int, gender:str='any', safe:bool=True, location:str='pt-BR'):
     '''
     Endpoint da API que retorna uma lista de usuários de acordo com a quantidade de registros(rows) passadas.
 
     Args:
-    - rows (int): quantidade de registros de usuários gerados,
-    - location (str): local de origem dos nomes, ruas, cidades, etc...
-
-    Exemplo de uso:
-    {url}/users?rows=15&location=en-US
-
-    Retorno:
-    {
-        'status_code': 200,
-        'users': {
-            'id': ?,
-            'first_name': ?,
-            'last_name': ?,
-            'email': ?,
-            'phone_number': ?,
-            'address': {
-                'number': ?,
-                'street_name': ?,
-                'city': ?,
-                'country': ?,
-                'postal_code': ?,
-            }
-        }
-    }
+    - rows(int): define a quantidade de usuários que serão retornados.
+    - gender(str): define o sexo do usuário criado, sendo 'f', 'm', ou 'any'.
+    - safe(bool): se o e-mail gerado é um e-mail não oficial ou não.
+    - locale(str): define a localização de origem dos dados ('pt-BR', 'en-US', etc...)
     '''
-    try:
-        response = await asyncio.to_thread(generate_user, rows, location)
-
-        if response['status_code'] == 500:
-            raise HTTPException(status_code=500, detail=response['description'])
-
-        return JSONResponse(response , status_code=200)
-    except Exception as e:
-        return JSONResponse({'description': str(e)}, status_code=500)
+    response = await asyncio.to_thread(Generator.generate_users, rows, gender, safe, location)
+    return JSONResponse(response , status_code=200)
--- src/helpers.py
+++ src/helpers.py
@@ -3,9 +3,15 @@ from faker import Faker
 
 class Generator:
     '''
-    Classe que armazena as instâncias de geradores Faker.
+    Classe que armazena as instâncias de geradores Faker e métodos de requisição de dados.
     '''
     _instances = {}
+
+    def _generate_email(first:str, last:str, safe:bool, fake:Faker):
+        '''
+        Gera um e-mail seguro (ou não) de acordo com o primeiro e último nome de uma pessoa
+        '''
+        return f'{first.lower().replace(' ', '')}.{last.lower().replace(' ', '')}@{fake.safe_domain_name() if safe else fake.domain_name()}'
 
     @staticmethod 
     def get_instance(locale:str='pt-BR') -> Faker:
@@ -13,25 +19,93 @@ class Generator:
         Método estático que retorna um generator de acordo com a sua localidade.
         Caso o generator não exista, o próprio método faz a instanciação e o armazena no atributo _instances.
         '''
-        if locale not in Generator._instances.values():
+        if locale not in Generator._instances:
             Generator._instances[locale] = Faker(locale=locale)
         return Generator._instances[locale]
 
-def generate_user(rows:int, locale:str='pt-BR'): 
-    '''
-    Função auxiliar que retorna um dicionário contendo os dados de um usuário.
-    '''
-    try:
-        fake = Generator.get_instance(locale)
+    @staticmethod
+    def generate_user(gender:str='any', safe:bool=True, locale:str='pt-BR'):
+        '''
+        Método estático que gera os dados de um único usuário, de acordo com o sexo.
 
-        data = {
-            'status_code': 200,
-            'users': [
-                {
-                    'id': i,
-                    'first_name': fake.first_name(),
-                    'last_name': fake.last_name(),
-                    'email': fake.safe_email(),
+        Args:
+        - gender(str): define o sexo do usuário criado, sendo 'f', 'm', ou 'any'.
+        - safe(bool): se o e-mail gerado é um e-mail não oficial ou não.
+        - locale(str): define a localização de origem dos dados ('pt-BR', 'en-US', etc...)
+        '''
+        try:
+            if gender not in ['f', 'm', 'any']:
+                raise KeyError("'gender' parameter should be 'm', 'f' or 'any'")
+
+            fake = Generator.get_instance(locale)
+            
+            genders = {
+                'f': fake.first_name_female(),
+                'm': fake.first_name_male(),
+                'any': fake.first_name(),
+            }
+
+            first = genders[gender]
+            last = fake.last_name()
+            email = Generator._generate_email(first, last, safe, fake)
+
+            data = {
+                'id': fake.random_int(),
+                'first_name': first,
+                'last_name': last,
+                'email': email,
+                'phone_number': fake.phone_number(),
+                'address': {
+                    'number': fake.building_number(),
+                    'street_name': fake.street_name(),
+                    'city': fake.city(),
+                    'country': fake.country(),
+                    'postal_code': fake.postcode(),
+                }
+            }
+
+            return data
+        except KeyError as err:
+            raise HTTPException(status_code=400, detail=str(err))
+        except Exception as err:
+            raise HTTPException(status_code=500, detail=str(err))
+
+    @staticmethod
+    def generate_users(rows:int, gender:str='any', safe:bool=True, locale:str='pt-BR'): 
+        '''
+        Método estático que gera os dados de n de usuários, de acordo com o valor definido em 'rows'.
+
+        Args:
+        - rows(int): define a quantidade de registros falsos de usuários
+        - gender(str): define o sexo do usuário criado, sendo 'f', 'm', ou 'any'.
+        - safe(bool): se o e-mail gerado é um e-mail não oficial ou não.
+        - locale(str): define a localização de origem dos dados ('pt-BR', 'en-US', etc...)
+        '''
+        try:
+            if rows <= 0:
+                raise ValueError("'rows' parameter should not be less or equal than 0")
+            if gender.lower() not in ['f', 'm', 'any']:
+                raise KeyError("'gender' paremeter should be 'f', 'm' or 'any'")
+            
+            fake = Generator.get_instance(locale)
+
+            genders = {
+                'f': fake.first_name_female,
+                'm': fake.first_name_male,
+                'any': fake.first_name
+            }
+
+            data = dict()
+            for i in range(rows):
+                first = genders[gender]()
+                last = fake.last_name()
+                email = Generator._generate_email(first, last, safe, fake)
+
+                data[i] = {
+                    'id': fake.random_int(),
+                    'first_name': first,
+                    'last_name': last,
+                    'email': email,
                     'phone_number': fake.phone_number(),
                     'address': {
                         'number': fake.building_number(),
@@ -41,12 +115,11 @@ def generate_user(rows:int, locale:str='pt-BR'):
                         'postal_code': fake.postcode(),
                     }
                 }
-                for i in range(rows)
-            ]
-        }
 
-        return data
-    except HTTPException as err:
-        return {'status_code': err.status_code, 'description': err.detail}
-    except Exception as err:
-        return {'status_code': 500, 'description': str(err)}
+            return data
+        except ValueError as err:
+            raise HTTPException(status_code=400, detail=str(err))
+        except KeyError as err:
+            raise HTTPException(status_code=400, detail=str(err))
+        except Exception as err:
+                raise HTTPException(status_code=500, detail=str(err))
